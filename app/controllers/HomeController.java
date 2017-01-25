f41a8349be608cf33267c7d0573dd1354eabf2ae
scottashipp <shipps@seattleu.edu>
2017-01-24 17:55:56 -0800

    Changes for part 3 video

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -14,40 +14,13 @@ import play.db.Database;
 import play.libs.Json;
 
 public class HomeController extends Controller {
-
-	private static final Element defaultElement = new Element("Search again", -1, "-", "-");
 	
-	private Database db;
-	
-	@Inject
-	public HomeController(Database db) {
-		this.db = db;
+	public HomeController() {
+		// empty
 	}
 	
-    public Result index() throws SQLException {
-        String searchTerm = request().getQueryString("element");
-        //TODO - query database
-        Connection conn = db.getConnection();
-        
-        String SQLQuery = "Select * from PERIODIC_ELEMENTS WHERE LOWER(ELEMENT) LIKE ?";
-        PreparedStatement getAnElementRow = conn.prepareStatement(SQLQuery);
-        
-        String queryParam = String.format("%%%s%%", searchTerm.toLowerCase());
-        getAnElementRow.setString(1, queryParam);
-        ResultSet theElementSet = getAnElementRow.executeQuery();
-        
-        //TODO - bind result to model
-        Element element = (theElementSet.next()) ? mapToElement(theElementSet) : defaultElement;
-        //TODO - convert model toJson
-        return ok(Json.toJson(element));
-    }
-    
-    private Element mapToElement(ResultSet theElementSet) throws SQLException {
-    	String element = theElementSet.getString("ELEMENT");
-    	int atomicNumber = theElementSet.getInt("ATOMIC_NUMBER");
-    	String symbol = theElementSet.getString("SYMBOL");
-    	String metalGroup = theElementSet.getString("METAL_GROUP");
-    	return new Element(element, atomicNumber, symbol, metalGroup);
+    public Result index() {
+    	return ok(views.html.element.render());
     }
 
 }
